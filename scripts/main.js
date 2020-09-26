85a6f32e014cb4cb8f60fa5e6f53725bad1de6e1
Joyce Kao <joyceykao@gmail.com>
2020-09-26 14:54:45 +0200

    added link update

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
 
 let results = document.querySelector("#results");
-const addtoList = (word) => {
+const addtoList = (link, name ) => {
   // results.empty();
-  const newListItem = `<li>${word}</li>`;
+  const newListItem = `<li><a href="${link}">${name}</a></li>`;
   results.insertAdjacentHTML("beforeend", newListItem);
 };
 
 const fetchUserString = () => {
-  fetch(`https://api.github.com/repos/openinnovationlifesciences/OILS-wiki/contributors`)
+  fetch(`https://api.github.com/orgs/openinnovationlifesciences/repos`)
   .then(response => response.json())
   .then((data) => {
-      addtoList(data[0].login);
+      addtoList(data[0].html_url, data[0].name);
     });
 };
 
