c9dcb7c4ae33fe4880abc0cb0d2d8dcbfc54a747
Joyce Kao <joyceykao@gmail.com>
2021-02-25 22:45:26 +0100

    added comments to JS functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
+// targets the results section on the page
 let results = document.querySelector("#results");
+
+//adds HTML to the results section for each workflow in the GH community
 const addtoList = (link, name, contributorInfo, updated, stars, watchers, description) => {
-  // results.empty();
   const newListItem =
     `
     <a class="card col-xs-12 col-sm-12 col-md-12 col-lg-3 d-flex align-items-center justify-content-around" id="workflow" target="_blank" href="${link}">
@@ -22,6 +24,7 @@ const addtoList = (link, name, contributorInfo, updated, stars, watchers, descri
   results.insertAdjacentHTML("beforeend", newListItem);
 };
 
+// Using the GH API, this function fetches information on all of the repositories on the FEZ community
 const fetchRepoString = () => {
   fetch(`https://api.github.com/orgs/FEZ-Finite-Element-Zurich/repos`)
   .then(response => response.json())
@@ -30,8 +33,12 @@ const fetchRepoString = () => {
     });
 };
 
+// calls the function above
 fetchRepoString();
 
+// takes the data from one repo from th fetchRepoString function to use the GH API to get the individual repo's information,
+// it uses the fetchContributorString function to get the contributor list for the specific repo
+// then feeds that info into the addtoList function
 const processRepoData = (html_url, name, owner_login, updated_at, watchers_count, stargazers_count, description, contributors_url) => {
         fetchContributorString(contributors_url)
           .then((contributorInfo) => {
@@ -40,17 +47,7 @@ const processRepoData = (html_url, name, owner_login, updated_at, watchers_count
           });
 };
 
-// const fetchContributorString = (contributorApi) => {
-//   fetch(contributorApi)
-//   .then(response=> response.json())
-//   .then((data) => {
-//     let contributorInfo = [];
-//     data.forEach(element => contributorInfo.push([element.login, element.url]));
-//     console.log(contributorInfo);
-//     return contributorInfo;
-//   });
-// }
-
+// uses the GH API to get contributor names of a specific repo
 let fetchContributorString = async (contributorApi) => {
   let response = await fetch (contributorApi);
   let data = await response.json();
@@ -59,6 +56,7 @@ let fetchContributorString = async (contributorApi) => {
   return contributorInfo;
 }
 
+// This function checks if the description exists or not and returns the appropriate HTML for the addtoList function above for the description section
 const formatDescription = (description) => {
   if (description == null) {
     return `<p class="container mb-1" style="height:25vh;"><em>None</em></p>`;
